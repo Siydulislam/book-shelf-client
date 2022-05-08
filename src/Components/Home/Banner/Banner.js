dea4dcf177d1433c0b7983d2561f02dad70ed608
Siydul Islam <siydulislam333@gmail.com>
2022-05-08 16:38:53 +0600

    header and banner done

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,10 +1,79 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import Banner1 from '../../../Assets/Images/banner-1.jpg';
+import Banner2 from '../../../Assets/Images/banner-2.jpg';
+import Banner3 from '../../../Assets/Images/banner-3.jpg';
+import Banner4 from '../../../Assets/Images/banner-4.jpg';
+import Banner5 from '../../../Assets/Images/banner-5.jpg';
 
 const Banner = () => {
     return (
-        <div>
-            Banner
-        </div>
+        <Carousel fade>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    style={{ height: "100vh" }}
+                    src={Banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    style={{ height: "100vh" }}
+                    src={Banner2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    style={{ height: "100vh" }}
+                    src={Banner3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    style={{ height: "100vh" }}
+                    src={Banner4}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    style={{ height: "100vh" }}
+                    src={Banner5}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
